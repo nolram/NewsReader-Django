d674aeda9c8dace9cf0c484a74feb6c86ca24372
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2015-06-23 18:19:24 -0300

    Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- NewsReaderDjango/settings.py
+++ NewsReaderDjango/settings.py
@@ -116,8 +116,8 @@ if USING_SQLITE:
 else:
     DATABASES = {
             'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2', #'django.db.backends.sqlite3',
-                'NAME': 'newsreader', #os.path.join(BASE_DIR, 'db.sqlite3'),
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'newsreader',
                 'USER': 'postgres',
                 'PASSWORD': '12345678',
             }
