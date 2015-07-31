8b89351d7c615e2eacd0ef448b50232dd703fc56
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2015-07-31 15:24:24 -0300

    Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- scripts/run_idle.py
+++ scripts/run_idle.py
@@ -1,8 +1,9 @@
 __author__ = 'nolram'
-from django.core.management.base import NoArgsCommand
+"""from django.core.management.base import NoArgsCommand
 
 from idlelib.PyShell import main
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         main()
+"""
